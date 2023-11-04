f4483f9dc1fa0b2f8fa83935cf4f2d0cd9a9072c
Alex Joedt <alex.joedt@tuta.io>
2023-11-04 12:02:32 +0100

    add go-version to compare restic versions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexjoedt/go-restic-wrapper
 
 go 1.20
+
+require github.com/hashicorp/go-version v1.6.0
--- restic.go
+++ restic.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"regexp"
+	"strings"
+
+	"github.com/hashicorp/go-version"
 )
 
 const (
@@ -28,10 +30,23 @@ func init() {
 		os.Exit(1)
 	}
 
-	versionRegex := regexp.MustCompile(minVersion)
-	if !versionRegex.MatchString(string(out)) {
+	fields := strings.Fields(string(out))
+	v, err := version.NewVersion(fields[1])
+	must(err)
+
+	minV, err := version.NewVersion(minVersion)
+	must(err)
+
+	if v.LessThan(minV) {
 		fmt.Printf("[FATAL] restic must be minimum version %s\n", minVersion)
 		os.Exit(1)
 	}
 
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
